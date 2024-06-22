6d4d3724001b65c153c8b18fb29d70a9e36be17d
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-06-22 18:45:50 +0900

    共用体とクラスの違い

---
+++ ch3/3.6/union.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+union U {
+  // デフォルトのアクセス指定子はpublic
+  float f;
+
+private:
+  int i;
+
+public:
+  U(); // コストラクターやデストラクターも定義できる
+  
+  int get_i() const;
+};
+
+// コンストラクターでは先頭以外のメンバー変数で初期化できる。
+U::U() : i(0xdeadbeef){
+  // メンバーのアドレスを列挙
+  std::cout << "&f: " << &f << std::endl
+            << "&i: " << &i << std::endl;
+}
+
+int U::get_i() const {
+  return this->i; // 共用体のメンバー関数もthisが使える
+}
+
+int main() {
+  U u; // デフォルトコンストラクターがint型で初期化する
+  std::cout << std::hex << u.get_i() << std::endl;
+  u.f = 2.71828f; // float型の値を代入する
+  std::cout << std::hex << u.get_i() << std::endl;
+  // -> 402df84d が出力される。　これはint型ではなくfloat型の値のビット表現で、int型としては壊れている。
+}
+
+
+
