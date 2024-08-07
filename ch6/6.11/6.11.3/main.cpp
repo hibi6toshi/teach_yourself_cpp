43113e839d3be330f479479f3549450e6aa667f8
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-08-08 00:46:53 +0900

    6.11.3 変換関数の注意点

---
+++ ch6/6.11/6.11.3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class A {
+public: 
+  void foo() { std::cout << "A::foo()" << std::endl; }
+};
+
+class B {
+  A a;
+
+public:
+  // メンバー変数のaを返す変換関数
+  operator A&() { return a; }
+};
+
+int main() {
+  B b;
+  b.foo(); // エラー　B::foo()が見つからない
+}
