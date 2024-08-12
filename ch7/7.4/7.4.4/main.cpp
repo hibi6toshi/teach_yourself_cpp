03f2e7e01df58c189cd955900248555532b51eb4
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-08-12 20:00:09 +0900

    7.4.4 仮想デストラクター

---
+++ ch7/7.4/7.4.4/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Base {
+public:
+  ~Base() { std::cout << "Base::~Base()" << std::endl;}
+};
+
+class Derived : public Base {
+public:
+  ~Derived() { std::cout << "Derived::~Derived()" << std::endl; }
+};
+
+Base* allocate() {
+  // 派生クラスを動的確保するが、関数の戻り値の型が基底クラスへのポインターになっているので、
+  // 暗黙変換によって基底クラスへのポインターが返される
+  return new Derived{};
+}
+
+int main() {
+  auto ptr = allocate(); // 基底クラスへのポインターが返される。
+
+  delete ptr; // 注意: デストラクターが仮想関数でない場合、派生クラスのデストラクターが呼ばれない
+  // Base::~Base()
+}
