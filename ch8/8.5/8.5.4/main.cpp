804c4abf6da35b853bb15792ba1b3249b9276463
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-08-25 06:53:18 +0900

    ## 8.5.4 ファイルの終端

---
+++ ch8/8.5/8.5.4/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+  std::ofstream out{"hello_eof.txt"};
+
+  out << "Hello, EOF!";
+
+  out.close();
+
+  std::ifstream in{"hello_eof.txt"};
+
+  std::string line;
+  // 1行読み込み
+  std::getline(in, line);
+
+  std::cout << line << std::endl;
+  std::cout << "ファイル末尾？" << std::boolalpha << in.eof() << std::endl;
+}
