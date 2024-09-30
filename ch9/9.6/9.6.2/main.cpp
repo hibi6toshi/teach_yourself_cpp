ee30f621352ad3ba492db96d033f77ae64b2a9b7
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-10-01 00:53:51 +0900

    9.6.2 型推論する非型テンプレートパラメーター

---
+++ ch9/9.6/9.6.2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// 非型テンプレートパラメーターを推論する
+template<auto i>
+void show_nttp() {
+  std::cout << "show_nttp<" << i << ">()" << std::endl;
+}
+
+int main() {
+  show_nttp<0>(); 
+  show_nttp<42>();
+}
