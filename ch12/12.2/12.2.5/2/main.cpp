d173c1bbef7717c9f91a66afa2a1d934f946ec69
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-10-16 22:23:02 +0900

    12.2.5 要素の削除

---
+++ ch12/12.2/12.2.5/2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector v = { 0, 1, 2, 3, 4 };
+
+  v.clear(); // 全要素を削除
+
+  for(int i : v) {
+    std::cout << i << std::endl;
+  }
+}
