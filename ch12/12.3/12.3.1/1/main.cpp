3633d81b579d7d8c28debc00f230ad5763c554d1
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-10-16 23:19:44 +0900

    12.3.1 std::listの基本動作

---
+++ ch12/12.3/12.3.1/1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+#include <iterator> // std::advance()用
+
+int main() {
+  // クラステンプレートの型推論を使って<T>を省略することもできる
+  std::list<int> l = { 1, 5, 9};
+
+  l.insert(l.begin(), 0);
+  l.insert(l.end(), 10);
+
+  auto iter = l.begin();
+  std::advance(iter, 2); // iterを2つ進める
+
+  l.insert(iter, 2, 3); // iterの位置に3を2つ挿入
+
+  int ia[] = { 6, 7, 8 };
+  std::advance(iter, -2); // iterを2つ戻す
+
+  l.insert(iter, ia, ia + 3); // iterの位置にaiの全要素を挿入
+
+  for (auto it = l.begin(); it != l.end(); ++it) {
+    std::cout << *it << std::endl;
+  }
+}
