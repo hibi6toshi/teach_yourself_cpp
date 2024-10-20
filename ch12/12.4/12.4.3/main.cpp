8cccd19956ed132342fcbcaa59f3d2c4a0342eca
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-10-20 16:43:29 +0900

    12.4.3 構造化束縛

---
+++ ch12/12.4/12.4.3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <tuple>
+
+int main() {
+  // クラステンプレートの型推論をさせる
+  // std::tuple<std::string, int, const char*>
+  std::tuple t{std::string{"structured"}, 42, "binding"};
+
+  auto [a, b, c] = t; // 構造化束縛。各要素をコピーする
+  
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+  std::cout << c << std::endl;
+
+  std::cout << std::endl;
+
+  auto& [x, y, z] = t; // 構造化束縛。ただし、各要素への参照を持つ
+
+  std::cout << x << std::endl;
+  std::cout << y << std::endl;
+  std::cout << z << std::endl;
+
+  x = "updated"; // 参照を使ってタプルの要素を変更する
+  std::cout << std::get<0>(t) << std::endl;
+}
